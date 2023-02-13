2d734d66f6cf6208592a40eb6b315228dfd2cecc
KurniawatiAgustina <kurniawatiagustina450@gmail.com>
2023-02-13 11:50:25 +0700

    edit welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Kurniawati Agustina</title>
+        <title>Laravel Kurniawati Agustina NIM 2141720040</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
